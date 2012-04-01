bca2bbe928df0df5e8decc42ad885d38daf04a33
kik <kik314@gmail.com>
2012-04-01 23:56:58 +0900

    view problem (wip)

--- AnarchyProofServer/Handler/Root.hs
+++ AnarchyProofServer/Handler/Root.hs
@@ -1,6 +1,7 @@
 module Handler.Root where
 
 import Import
+import Data.Text as T
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -22,11 +23,18 @@ getProblemListR = do
         h2id <- lift newIdent
         setTitle "AnarchyProofServer homepage"
         $(widgetFile "homepage")
-  
+
 getProblemViewR :: ProblemId -> Handler RepHtml
 getProblemViewR problem_id = do
   problem <- runDB $ get404 problem_id
   defaultLayout $ do
     setTitle "AnarchyProofServer homepage"
+    let descLines = T.lines $ problemDescription problem
+    let defsLines = T.lines $ problemDefinitions problem
+    let hasDefs = defsLines /= []
+    let thmLines  = T.lines $ problemTheorem problem
+    let verfLines = T.lines $ problemVerifier problem
+    let assmLines = T.lines $ problemAssumption problem
+    let hasAssm = assmLines /= []
     $(widgetFile "problem-view")
   
