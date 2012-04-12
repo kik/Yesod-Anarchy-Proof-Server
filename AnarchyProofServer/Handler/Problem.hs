ab47fc2187128d8b4b94534f60883691280744ee
kik <kik314@gmail.com>
2012-04-12 16:11:09 +0900

    ans form

--- AnarchyProofServer/Handler/Problem.hs
+++ AnarchyProofServer/Handler/Problem.hs
@@ -21,27 +21,26 @@ textWidget t =
 
 data Ans = Ans
            { ansUser :: Text
-           , ansLang :: LanguageId
+           , ansLang :: Entity Language
            , ansFile :: FileInfo
            }
            
-ansForm :: [Entity Language] -> Form Ans
-ansForm ls = renderDivs $ Ans
-             <$> areq textField "User" Nothing
-             <*> areq langField "Lang" Nothing
-             <*> fileAFormReq "File"
-               where
-                 langField = selectFieldList 
-                             $ map nameValue ls
-                 nameValue (Entity k v) =
-                   (languageName v, k)
+ansForm :: Form Ans
+ansForm =
+  renderDivs $ Ans
+  <$> areq textField "User" Nothing
+  <*> areq langField "Lang" Nothing
+  <*> fileAFormReq "File"
+    where
+      langOpts = optionsPersist [] [Asc LanguageId] languageName
+      langField = selectField langOpts
 
 getProblemViewR :: ProblemId -> Handler RepHtml
 getProblemViewR problemId = do
   problem <- runDB $ get404 problemId
   answers <- runDB $ selectList [AnswerProblemId ==. problemId] [Asc AnswerSize, Asc AnswerCreatedAt]
   langs   <- runDB $ selectList [] [Asc LanguageId]
-  (ansWidget, ansEnctype) <- generateFormPost $ ansForm langs
+  (ansWidget, ansEnctype) <- generateFormPost $ ansForm
   defaultLayout $ do
     setTitle "AnarchyProofServer homepage"
     let desc = textWidget $ problemDescription problem
@@ -55,3 +54,21 @@ getProblemViewR problemId = do
       [(l, groupByLang lk as) | Entity lk l <- ls]
     groupByLang lk as =
       [a | Entity _ a <- as, answerLanguageId a == lk]
+
+postProblemSolveR :: ProblemId -> Handler RepHtml
+postProblemSolveR problemId = do
+  problem <- runDB $ get404 problemId
+  ((result, _), _) <- runFormPost ansForm
+  answer <- case result of
+        FormSuccess r -> return r
+        _ -> redirect $ ProblemViewR problemId
+  $(logDebug) $ ansUser answer
+  liftIO $ checkAns problem answer
+  defaultLayout $ do
+    setTitle "AnarchyProofServer homepage"
+    [whamlet|TODO|]
+
+checkAns :: Problem -> Ans -> IO ()
+checkAns problem answer = do
+  return ()
+  
